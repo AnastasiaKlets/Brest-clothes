6026e8ce32868bc0d5c73b1e2b143979371b25bc
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-05-22 14:27:11 +0300

    add all

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-100px";
-    }
-    prevScrollpos = currentScrollPos;
-}
-
-let btn = document.querySelector('.btn');
-let blockHidden = document.querySelector('.block');
-function showBlock() {
-    if(blockHidden.classList.contains('b-show')){
-        blockHidden.classList.remove('b-show');
-    }
-    else {
-        blockHidden.classList.add('b-show');
-    }
-}
-btn.addEventListener('click', showBlock);
-
-
 function tabs(tabsContainerSelector, tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
     let tabs = document.querySelectorAll(`${tabsContainerSelector} ${tabsSelector}`),
         tabsContent = document.querySelectorAll(`${tabsContainerSelector} ${tabsContentSelector}`),
@@ -484,3 +460,25 @@ document.querySelector('.consult_form').addEventListener('submit', (e) => {
     openModal('.thanks');
 });
 
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+let btn = document.querySelector('.btn');
+let blockHidden = document.querySelector('.block');
+function showBlock() {
+    if(blockHidden.classList.contains('b-show')){
+        blockHidden.classList.remove('b-show');
+    }
+    else {
+        blockHidden.classList.add('b-show');
+    }
+}
+btn.addEventListener('click', showBlock);
